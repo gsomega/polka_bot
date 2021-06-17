7466850bf62e9a204741d0335e95771192faaaef
gsomega <gsomega@users.noreply.github.com>
2021-06-16 21:34:19 -0700

    first commit, a standard deck and some player functions

---
+++ CardTypes.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class Suit(Enum):
+    CLUB = "CLUB"
+    SPADE = "SPADE"
+    DIAMOND = "DIAMOND"
+    HEART = "HEART"
+
+class Rank(Enum):
+    TWO = "TWO"
+    THREE = "THREE"
+    FOUR = "FOUR"
+    FIVE = "FIVE"
+    SIX = "SIX"
+    SEVEN = "SEVEN"
+    EIGHT = "EIGHT"
+    NINE = "NINE"
+    TEN = "TEN"
+    JACK = "JACK"
+    QUEEN = "QUEEN"
+    KING = "KING"
+    ACE = "ACE"
+
