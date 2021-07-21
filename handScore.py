4c24afd0f8e296069de101ee66a3351ba6a44eaa
gsomega <gsomega@users.noreply.github.com>
2021-07-21 15:05:53 -0700

    hand_score_done, needs more testing

--- handScore.py
+++ handScore.py
@@ -41,55 +41,58 @@ def x_of_a_kind(hand: List[Card]):
     if 4 in counts:
         kind = counts.index(4)
         kicker = counts.index(1)
-        return [3,kind,kicker] #4 of a kind
+        return [3, kind, kicker]  # 4 of a kind
 
     elif 3 in counts:
         trip = counts.index(3)
         if 2 in counts:
             pair = counts.index(2)
-            return [4,trip,pair] #full house
+            return [4, trip, pair]  # full house
         else:
             kicker2 = counts.index(1)
-            kicker1 = counts[kicker2+1:].index(1)
-            return [7,trip, kicker1, kicker2] #3 of a kind
+            kicker1 = counts[kicker2+1:].index(1) + kicker2 + 1
+            return [7, trip, kicker1, kicker2]  # 3 of a kind
 
     elif 2 in counts:
         pair = counts.index(2)
         if 2 in counts[pair+1:]:
-            pair2 = counts[pair+1:].index(2)
+            pair2 = counts[pair+1:].index(2)+(pair+1)
             kicker = counts.index(1)
-            return [8,pair2,pair,kicker] #2 pair
+            return [8, pair2, pair, kicker]  # 2 pair
         else:
-            kicker3 = counts.index(1)
-            kicker2 = counts[kicker3+1:].index(1)
-            kicker1 = counts[kicker2+1:].index(1)
-            return [9, pair, kicker1,kicker2,kicker3] # pair
-    else: #High card
-        kickers = [i for i,x in enumerate([counts]) if x==1]
-        return [10].extend(kickers[::-1])
+            kickers = [i for i, x in enumerate(counts) if x == 1]
+            result = [9, pair]
+            result.extend(kickers[::-1])
+            return result  # pair
+    else:  # High card
+        kickers = [i for i, x in enumerate(counts) if x==1]
+        result = [10]
+        result.extend(kickers[::-1])
+        return result
 
 
 def hand_score(hand: List[Card]):
-        hand.sort(key=lambda x: x.number)
-        result = []
-        Flush = is_flush(hand)
-        Straight = is_straight(hand)
-        Kinds = x_of_a_kind(hand)
-        if Flush and Straight:
-            if hand[3].number == 13:
-                return [1] #royal flush
-            else:
-                return [2,hand[3].number] #straight flush
-        elif Kinds[0]==3 or Kinds[0]==4:
-            return Kinds
-        elif Flush:
-            kickers = [i.number for i in hand]
-            result = [5].extend(kickers[::-1])
-            return result
-        elif Straight:
-            return [6, hand[3].number]  # straight
+    hand.sort(key=lambda x: x.number)
+    result = []
+    flush = is_flush(hand)
+    straight = is_straight(hand)
+    kinds = x_of_a_kind(hand)
+    if flush and straight:
+        if hand[3].number == 13:
+            return [1]  # royal flush
         else:
-            return Kinds
+            return [2, hand[3].number]  # straight flush
+    elif kinds[0] == 3 or kinds[0] == 4:
+        return kinds
+    elif flush:
+        kickers = [i.number for i in hand]
+        result = [5]
+        result.extend(kickers[::-1])
+        return result
+    elif straight:
+        return [6, hand[3].number]  # straight
+    else:
+        return kinds
 
 def best_hand(board, hand):
     pass
